b349e426937aef3b9bed5e9459abd1f0e429f006
ITStudentFromFinland <ihanokvitsiniekka@gmail.com>
2020-04-11 00:25:57 +0300

    added bottom navigation bar

--- app/src/main/java/com/example/travelplanner/MainActivity.java
+++ app/src/main/java/com/example/travelplanner/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.travelplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    BottomNavigationView navView = findViewById(R.id.nav_view);
+    View mTextMessage = findViewById(R.id.message);
+    navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+          case R.id.navigation_home:
+            break;
+
+          case R.id.navigation_dashboard:
+
+            break;
+
+          case R.id.navigation_notifications:
+            break;
+        }
+        return false;
+      }
+    });
   }
 }
